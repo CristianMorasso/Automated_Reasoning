c87ee02543b7e85d19b307019444c96691af31ca
CristianMorasso <morassocristian0@gmail.com>
2023-06-22 14:02:58 +0200

    verbose parameter

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ import congruence_closure as cc
 import smt_parse as smtp
 
 def main(file="",term=None):
+    verbose = False
     # Checks
     if len(sys.argv)>1: file = sys.argv[1]
+
+    if len(sys.argv)>2:
+        verbose = sys.argv[2] == "verbose"
+   
     if file == "": 
         print("ERROR: no input file!")
         exit()
@@ -26,35 +31,39 @@ def main(file="",term=None):
         if or_presence:
             token = token[1:-1]
         equations,atoms = smt_parser.parse_and_clause(token) 
-        print(equations)
+        #print(equations)
         # Drawing the graph in the CC_DAG object instance
         atom_parser.parse(atoms) 
         solver.add_fathers()  
         
         G = solver.create_dag_to_visualize()
-        solver.visualize_dag(G)
+        if verbose:
+            solver.visualize_dag(G)
         status_before_process = solver.graph_to_string()
         
         # Parsing the formulas and transforming them in tuples for the CC algorithm 
         solver.equalities, solver.inequalities = gp.parse_equations(equations,atom_parser.atom_dict) 
         # Running Congruence Closure 
         result, ineq = solver.solve() 
-        print((f"Problem: "))
         print((f"Formulas:\n{equations}"))
-        print(f"Graph Nodes before the process:\nID\tAtom\tCCPAR\n{status_before_process}")
-        print(f"Processed Graph Nodes:\nID\tAtom\tCCPAR\n{solver.graph_to_string()}")
-        print(solver.print_formulas())
-        if result:
-            print(f"UNSAT, Inequality -> Find version: ({solver.graph[ineq[0]].string} = {solver.graph[ineq[1]].string}) -> ({solver.graph[solver.graph[ineq[0]].find].string} != {solver.graph[solver.graph[ineq[1]].find].string})\n")
-        else:
-            print("SAT")
+        if verbose:
+            #print((f"Problem: "))
+            #print((f"Formulas:\n{equations}"))
+            print(f"Graph Nodes before the process:\nID\tAtom\tCCPAR\n{status_before_process}")
+            print(f"Processed Graph Nodes:\nID\tAtom\tCCPAR\n{solver.graph_to_string()}")
+            print(solver.print_formulas())
+            if result:
+                print(f"UNSAT, Inequality -> Find version: ({solver.graph[ineq[0]].string} = {solver.graph[ineq[1]].string}) -> ({solver.graph[solver.graph[ineq[0]].find].string} != {solver.graph[solver.graph[ineq[1]].find].string})\n")
+            else:
+                print("SAT")
         if not result: 
             print("SAT")
             break
-        print(f"Unsat: {token}\n\n\n")
+        print(f"UNSAT\n")
 
-    print(f"Ground Truth: {ground_truth}")
-    solver.visualize_dag(G, find = True)
+        print(f"Ground Truth: {ground_truth}\n\n")
+    if verbose:    
+        solver.visualize_dag(G, find = True)
     return result 
 
 if __name__ == "__main__": 
